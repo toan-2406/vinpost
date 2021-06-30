3a2139d3e83dd847fc9c81863c328dcafd130d04
toan-2406 <57821796+toan-2406@users.noreply.github.com>
2021-06-30 18:06:37 +0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,68 @@
+<div class="footer-big">
+                <div class="container">
+                    <div class="footer-company">
+                        <div class="footer-logo">
+                            VINPOST
+                        </div>
+                        <p><b>Vinpost</b> là doanh nghiệp hàng đầu cung cấp dịch vụ chuyển phát nhanh, hàng hóa, bưu
+                            kiện trong nước, quốc tế tại Việt Nam.</p>
+                        <hr>
+                        <h3>Thông tin liên hệ</h3>
+                        <ul>
+                            <li>
+                                <i class="fas fa-phone"></i>
+                                <p>Giấy chứng nhận đăng kí kinh doanh số</p>
+                            </li>
+                            <li>
+                                <i class="fas fa-phone"></i>
+                                <p>Giấy chứng nhận đăng kí kinh doanh số</p>
+                            </li>
+                            <li>
+                                <i class="fas fa-phone"></i>
+                                <p>Giấy chứng nhận đăng kí kinh doanh số</p>
+                            </li>
+                            <li>
+                                <i class="fas fa-phone"></i>
+                                <p>Giấy chứng nhận đăng kí kinh doanh số</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-intruduction">
+                        <h3>Giới thiệu</h3>
+                        <ul>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-support">
+                        <h3>Hỗ trợ khách hàng</h3>
+                        <ul>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                            <li><a href="#"><p>text</p></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-contact">
+                        <h3>Hỗ trợ</h3>
+                        <p>Liên hệ Hotline: <b>7:00 - 20:00</b></p>
+                        <h3>Kết nối</h3>
+                        <div class="footer-social">
+                            <i class="fab fa-facebook-square"></i>
+                            <i class="fab fa-facebook-square"></i>
+                            <i class="fab fa-facebook-square"></i>
+                            <i class="fab fa-facebook-square"></i>
+                        </div>
+                        <img src="/img/Rfefde4cb941850f336ec90964cab625f.png" alt="">
+                        <p>Đã thông báo bộ công thương</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="copyright">copyright 2021 by Vinpost</div>
---
+++ header.php
@@ -0,0 +1,47 @@
+<div class="header-top">
+                <div class="container">
+                    <div class="header-contact">
+                        <span>
+                            <h3>Hotline: </h3>
+                            <p>03458xxxx</p>
+                        </span>
+                    </div>
+                    <div class="header-login">
+                        <span><a href="#" class="login">Đăng nhập </a>
+                            /
+                            <a href="#" class="login"> Đăng kí</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div id="sticky-menu" class="header-main">
+                <div class="container">
+                    <div class="header-logo">
+                        <img src="https://fakeimg.pl/250x130/" alt="">
+                    </div>
+                    <div class="header-menu">
+                        <ul class="header-menu__list">
+                            <li class="header-menu__item">
+                                <a href="index.html" class="header-menu__link active">
+                                    Trang chủ
+                                </a>
+                            </li>
+                            <li class="header-menu__item">
+                                <a href="index.html" class="header-menu__link">
+                                    Tra đơn hàng
+                                </a>
+                            </li>
+                            <li class="header-menu__item">
+                                <a href="index.html" class="header-menu__link">
+                                    Dịch vụ
+                                </a>
+                            </li>
+                            <li class="header-menu__item">
+                                <a href="index.html" class="header-menu__link">
+                                    Liên hệ chúng tôi
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
